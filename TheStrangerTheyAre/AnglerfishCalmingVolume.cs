02f24274c07e24bdd7cdd22c429bcb502cc395c2
AnonymousStrangerOW <169866456+AnonymousStrangerOW@users.noreply.github.com>
2025-01-30 13:07:19 -0500

    final stretch update 5
    - update some signals to not have very long detection volumes
    - made a certain house safe from abyssal dangers
    - changed some dialogue and shiplogs to be less spoilery about a certain endgame area.
    - mechanic made much more useful in an abyssal danger zone
    - a few more stuff

---
+++ TheStrangerTheyAre/AnglerfishCalmingVolume.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using NewHorizons.Utility;
+
+namespace TheStrangerTheyAre
+{
+    public class AnglerfishCalmingVolume : MonoBehaviour
+    {
+        private AnglerfishController[] anglerControllers = new AnglerfishController[2];
+        private SocketedQuantumObject[] anglerQuantumControllers = new SocketedQuantumObject[2];
+
+        void Start()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                anglerControllers[i] = SearchUtilities.Find("BASE_ANGLER" + (i + 1)).GetComponent<AnglerfishController>();
+                anglerQuantumControllers[i] = SearchUtilities.Find("BASE_ANGLER" + (i + 1)).GetComponent<SocketedQuantumObject>();
+            }
+            
+        }
+
+        public virtual void OnTriggerEnter(Collider hitCollider)
+        {
+            //checks if player collides with the trigger volume
+            if (hitCollider.CompareTag("PlayerDetector") && enabled)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    anglerQuantumControllers[i].ChangeQuantumState(true);
+                    anglerControllers[i].ChangeState(AnglerfishController.AnglerState.Lurking);
+                }
+            }
+        }
+    }
+}
