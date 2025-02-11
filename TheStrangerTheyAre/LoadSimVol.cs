312366da6461918165f1c7b94cbe1dae8705a785
AnonymousStrangerOW <169866456+AnonymousStrangerOW@users.noreply.github.com>
2025-02-11 15:42:45 -0500

    v1.1.0 first hotfix update!
    - fixes all the weird crashing and gravity bugs related to dying in the simulation
    - a certain area is now not visible in the real world
    - fixed all the "more to explore" in areas that shouldn't, except for one in a distant, mysterious planet (i have no idea why that one's broken)
    - you can no longer get stuck in open crates in a certain area without a suit on
    - several minor dialogue and shiplog edits

--- TheStrangerTheyAre/LoadSimVol.cs
+++ TheStrangerTheyAre/LoadSimVol.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using NewHorizons.Utility;
+using OWML.ModHelper;
 
 namespace TheStrangerTheyAre
 {
@@ -10,10 +11,13 @@ namespace TheStrangerTheyAre
         private Sector simSector; // for storing the sim sector
         private Sector dz4Sector; // for storing the dz4 sector
         private DreamCampfireStreaming dz4Stream; // for storing the dream campfire streaming script
+        public bool hasEnteredVolume;
 
         void Start()
         {
+            hasEnteredVolume = false;
             player = Locator.GetPlayerSectorDetector(); // get player's sector detector
+            GlobalMessenger.AddListener("ExitDreamWorld", OnExitDreamWorld); // checks if player leaves the sim
             simSector = SearchUtilities.Find("DreamWorld_Body/Sector_DreamWorld").GetComponent<Sector>(); // get sim root sector
             dz4Sector = SearchUtilities.Find("DreamWorld_Body/Sector_DreamWorld/Sector_DreamZone_4").GetComponent<Sector>(); // get dz4 root sector
             dz4Stream = SearchUtilities.Find("RingWorld_Body/Sector_RingInterior/Sector_Zone4/PrisonDocks/Sector_PrisonInterior/Interactibles_PrisonInterior/Prefab_IP_DreamCampfire/DreamCampfire_Streaming").GetComponent<DreamCampfireStreaming>(); // get dream campfire streaming script
@@ -24,10 +28,16 @@ namespace TheStrangerTheyAre
             //checks if player collides with the trigger volume
             if (hitCollider.CompareTag("PlayerDetector") && enabled)
             {
+                hasEnteredVolume = true;
                 simSector.AddOccupant(player); // adds player to sim root sector
                 dz4Sector.AddOccupant(player); // adds player to sim dz4 sector
                 dz4Stream.UpdatePreloadingState(true); // runs the streamer for dz4 manually
             }
+        }
+
+        void OnExitDreamWorld()
+        {
+            hasEnteredVolume = false;
         }
     }
 }
