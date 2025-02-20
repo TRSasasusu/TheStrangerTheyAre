ced67ac6455a83222bd5c90ff149b7ea080c3be6
AnonymousStrangerOW <169866456+AnonymousStrangerOW@users.noreply.github.com>
2025-02-20 09:53:34 -0500

    v1.4.0 hotfix
    - the simulation interactibles should actually work this time
    - updated some ship log entry photos i realized never got updated
    - added a sign in a certain caved area to push players in the right direction
    - all solanum-related bugs fixed
    - edited some dialogue and ship logs yet again (of course). i urge potential translators to continue re-generating the english.json after each update.

---
+++ TheStrangerTheyAre/SolanumDialogueFix.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using NewHorizons.Utility;
+using NewHorizons.Components.Props;
+
+namespace TheStrangerTheyAre;
+
+public class SolanumDialogueFix : MonoBehaviour
+{
+    private GameObject solanumDialogue;
+
+    private void Start()
+    {
+        solanumDialogue = SearchUtilities.Find("QuantumMoon_Body/Sector_QuantumMoon/State_EYE/Interactables_EYEState/ConversationPivot/Character_NOM_Solanum/TSTA_Solanum");
+    }
+
+    private void Update()
+    {
+        if (DialogueConditionManager.SharedInstance.GetConditionState("TSTA_SOL_SCANNED") == true)
+        {
+            solanumDialogue.SetActive(true);
+        } else
+        {
+            solanumDialogue.SetActive(false);
+        }
+    }
+}
