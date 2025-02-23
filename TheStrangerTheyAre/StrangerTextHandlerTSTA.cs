7e2e710f83d9ec40365fc3c5d80cc141cb45ef93
AnonymousStrangerOW <169866456+AnonymousStrangerOW@users.noreply.github.com>
2025-02-23 12:09:37 -0500

    v2.0.0 the raft update
    - adds rafts and raft docks to three certain planets
    - the text in the stranger is now readable, as many requested
    - title screen now has music, ambience, and more green!
    - made a certain secret area even harder to find
    - fixed more shiplogs including pictures and entries
    - more dialogue edits to make puzzles easier to understand
    - you can now invite a certain someone somewhere even if you used a vision torch
    - many more

---
+++ TheStrangerTheyAre/StrangerTextHandlerTSTA.cs
@@ -0,0 +1,31 @@
+﻿using NewHorizons.Utility;
+using UnityEngine;
+
+namespace TheStrangerTheyAre
+{
+    public class StrangerTextHandlerTSTA : MonoBehaviour
+    {
+        private GameObject[] strangerDialogue = new GameObject[11]; // create new array of gameobjects to store all custom sim reels
+
+        private void Start()
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                strangerDialogue[i] = SearchUtilities.Find("TSTA_StrangerDialogue_" + (i + 1)); // gets all custom strangerDialogue in the sim, stores in array
+            }
+        }
+
+        private bool KnowsLanguage()
+        {
+            return Locator.GetShipLogManager().IsFactRevealed("ANGLERS_EYE_ALIENTEXT_E2");
+        }
+
+        private void Update()
+        {
+            foreach (GameObject dialogue in strangerDialogue)
+            {
+                dialogue.SetActive(KnowsLanguage());
+            }
+        }
+    }
+}
--- TheStrangerTheyAre/TheStrangerTheyAre.cs
+++ TheStrangerTheyAre/TheStrangerTheyAre.cs
@@ -46,24 +46,6 @@ namespace TheStrangerTheyAre
             Instance.ModHelper.Console.WriteLine(text, messageType);
         }
 
-        private void PlaceTitlePlanet()
-        {
-            var background = GameObject.Find("Background");
-            var planetpivot = GameObject.Find("PlanetPivot");
-            planetpivot.FindChild("Prefab_HEA_Campfire").SetActive(false);
-            planetpivot.FindChild("PlanetRoot").SetActive(false);
-
-            if (_homeMoonBundle == null)
-            {
-                _homeMoonBundle = ModHelper.Assets.LoadBundle("assets/AssetBundle/title");
-            }
-            GameObject homemoon = Instantiate(_homeMoonBundle.LoadAsset<GameObject>("Assets/NewTitlePlanet.prefab"));
-            AssetBundleUtilities.ReplaceShaders(homemoon);
-            homemoon.transform.parent = planetpivot.transform;
-            homemoon.transform.localPosition = new Vector3(3.4349f, - 12.3667f, 3.1489f);
-            homemoon.transform.localRotation = Quaternion.Euler(333.0655f, 8.4042f, 7.4983f);
-        }
-
         private void Start()
         {
             // Starting here, you'll have access to OWML's mod helper.
@@ -92,7 +74,6 @@ namespace TheStrangerTheyAre
                 ModHelper.Console.WriteLine("IMPERIALISM DETECTED! SHUTTING DOWN!", MessageType.Error);
                 Application.Quit();
             }
-            PlaceTitlePlanet();
             LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
             {
                 if (loadScene == OWScene.PostCreditsScene)
@@ -105,15 +86,7 @@ namespace TheStrangerTheyAre
                             EndSceneAddition.LoadEndingAdditions(endingBundle);
                         }
                     }
-                } else { 
-                    // unload when not on title screen
-                    if (_homeMoonBundle != null)
-                    {
-                        _homeMoonBundle.Unload(true);
-                        _homeMoonBundle = null;
-                    }
                 }
-
                 if (loadScene != OWScene.SolarSystem) return;
                 ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
             };
