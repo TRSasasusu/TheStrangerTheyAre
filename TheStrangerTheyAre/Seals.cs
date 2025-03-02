a6af386e76f338a577c38c1fe5b3ed5e88fbb6b1
AnonymousStrangerOW <169866456+AnonymousStrangerOW@users.noreply.github.com>
2025-03-02 00:41:23 -0500

    updated some shiplog stuff for wyrm to test
    - you now learn about the seals when you pick them up instead of hitting them
    - added a component for possible changes to quantum entanglement to directly work with nh

--- TheStrangerTheyAre/Seals.cs
+++ TheStrangerTheyAre/Seals.cs
@@ -28,6 +28,26 @@ public class Seals : OWItem
     public override void PickUpItem(Transform holdTranform)
     {
         base.PickUpItem(holdTranform);
+        switch (sealName)
+        {
+            case "Sizzling Sands":
+                Locator.GetShipLogManager().RevealFact("DESERT_SEAL_E");
+                break;
+            case "Ringed Giant":
+                Locator.GetShipLogManager().RevealFact("RING_SEAL_E");
+                break;
+            case "Velvet Vortex":
+                Locator.GetShipLogManager().RevealFact("CRIMSON_SEAL_E");
+                break;
+            case "Burning Bombardier":
+                Locator.GetShipLogManager().RevealFact("VOLCANO_SEAL_E");
+                break;
+            case "Distant Enigma":
+                Locator.GetShipLogManager().RevealFact("QUANTUM_SEAL_E");
+                break;
+            default:
+                break;
+        }
     }
 
     public override void SocketItem(Transform socketTransform, Sector sector)
