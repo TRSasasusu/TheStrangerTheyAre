725c29a003086b49defefae223f208e599f3d96c
AnonymousStrangerOW <169866456+AnonymousStrangerOW@users.noreply.github.com>
2025-04-10 20:18:09 -0400

    v2.4.3 Hotfix
    - you can no longer fly over a certain deadly windstorm and cheese half the mod
    - slate now gives you advice on how to land on a certain planet
    - english.json and diff.txt updated for translators, new slate dialogue needs translating.
    - the absence of achievement names and descriptions has been fixed

--- TheStrangerTheyAre/FuckOffSlate.cs
+++ TheStrangerTheyAre/FuckOffSlate.cs
@@ -5,24 +5,30 @@ namespace TheStrangerTheyAre
 {
     public class FuckOffSlate : MonoBehaviour
     {
-        GameObject evilDialogue;
+        // variables to store the slate remote dialogues
+        private GameObject evilDialogue;
+        private GameObject evilDialogue2;
 
         void Start()
         {
-            evilDialogue = SearchUtilities.Find("TimberHearth_Body/Sector_TH/Sector_Village/Sector_StartingCamp/Characters_StartingCamp/Villager_HEA_Slate/TSTA_SlateRemote");
+            evilDialogue = SearchUtilities.Find("TimberHearth_Body/Sector_TH/Sector_Village/Sector_StartingCamp/Characters_StartingCamp/Villager_HEA_Slate/TSTA_SlateRemote"); // dialogue used to tell the player about a strange signal in the village
+            evilDialogue2 = SearchUtilities.Find("TimberHearth_Body/Sector_TH/Sector_Village/Sector_StartingCamp/Characters_StartingCamp/Villager_HEA_Slate/TSTA_SlateRemote2"); // dialogue used to tell the player about using the scout to land on angler's eye
         }
 
         void Update()
         {
-            if (Check() && evilDialogue != null)
+            if (Locator.GetShipLogManager().IsFactRevealed("HEARTH_VISION") && evilDialogue != null) // checks if the player saw the vision on timber hearth and if the dialogue volume is null
             {
-                Destroy(evilDialogue);
+                Destroy(evilDialogue); // destroys the signal hint dialogue
             }
-        }
 
-        private bool Check()
-        {
-            return Locator.GetShipLogManager().IsFactRevealed("HEARTH_VISION");
+            if (evilDialogue2 != null) // checks if dialogue volume is null
+            {
+                if (!Locator.GetShipLogManager().IsFactRevealed("ANGLERS_EYE_MAIN_E") || PlayerData.GetPersistentCondition("SLATE_HASYELLED")) // checks if the player hasn't been to angler's eye yet or hasn't already been yelled at by slate about using the scout to land.
+                {
+                    Destroy(evilDialogue2); // destroys the scout-landing dialogue
+                }
+            }
         }
     }
 }
